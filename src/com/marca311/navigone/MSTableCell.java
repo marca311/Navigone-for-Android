6e47faa12cd38c5c2fc94bc9630b44d1e8afefb2
Marcus Dyck <marcusjbdyck@gmail.com>
2012-12-25 00:02:44 -0600

    Worked on UI. A lot more to go.

---
+++ src/com/marca311/navigone/MSTableCell.java
@@ -0,0 +1,16 @@
+package com.marca311.navigone;
+
+public class MSTableCell {
+	private String time = null;
+	private String instruction = null;
+	
+	//Methods
+	//Constructor method
+	public MSTableCell(String theTime, String theInstruction) {
+		time = theTime;
+		instruction = theInstruction;
+	}
+	
+	//Getter methods
+	//Nothing here, I need to figure out the structure for the table
+}
